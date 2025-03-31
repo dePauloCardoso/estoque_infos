9dd586311c80cd8c2f3a2e649ddb87e099f5a18e
Paulo Cardoso <cardosopaulohenrique91@gmail.com>
2025-03-31 15:42:13 -0300

    rm db_config

--- db_config.py
+++
@@ -1,5 +0,0 @@
-# Configurações de conexão ao banco de dados
-DB_SERVER = '10.129.0.191'
-DB_DATABASE = 'PROTHEUS_PROD'
-DB_USERNAME = 'consulta'
-DB_PASSWORD = 'consulta'  # Use uma maneira segura de armazenar senhas
